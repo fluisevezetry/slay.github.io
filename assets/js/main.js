fa6aef22a2a00c9296431c4c746486799ffec45f
OukiLove <84971861+OukiLove@users.noreply.github.com>
2022-05-07 23:03:54 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".preloader-content_button");
 let animInterval;
 let loadInterval;
 
-const videoId = "x6M2Ih3ZZHk"
+const videoId = "KL0SJm4qkGg"
 YT.ready(_=>{
     window.player = new YT.Player("player", {
         videoId: videoId,
